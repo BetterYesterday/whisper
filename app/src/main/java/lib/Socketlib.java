65c88e36838dc4551c25d686b61e035e022cc703
clp <clplanet26@gmail.com>
2016-05-02 17:26:25 +0900

    maybe.. socketlib completed?

--- app/src/main/java/lib/Socketlib.java
+++ app/src/main/java/lib/Socketlib.java
@@ -16,20 +16,16 @@ public class Socketlib implements Runnable {
 	OutputStream out;
 	DataOutputStream dos;
 	private String serverip;
-	private int selection;
-	private StringBuffer received = new StringBuffer();
+	private int serverport;
 	Thread[] t;
-	private StringBuffer sending = new StringBuffer();
-	private String message;
+	private byte[] message;
+	private byte bs[];
 
-	public Socketlib(String serverIp) {
+	public Socketlib(String serverIp, int serverport) {
 		serverip = serverIp;
+		this.serverport = serverport;
+		
 		t = new Thread[2];
-	}
-
-	public void selection(int input) {// 인풋 값을 통해 해당 쓰레드에 필요한 기능을 지시.
-		selection = input;
-
 	}
 
 	public void Connect() {// 인풋 값 1
@@ -45,58 +41,71 @@ public class Socketlib implements Runnable {
 	}
 
 	public void DisConnect() {// 인풋 값 2
+		t[2].start();
+	}
+
+	public void Sendmessage(String send) {// 메시지 송신(String, 암호화 필요하지 않을 시 사용
+		message = send.getBytes();
+		t[1].start();
+
+	}public void Sendmessage(byte[] send) {// 메시지 송신(byte 자료형. 암호화 필요 시 사용
+		message = send;
+		t[1].start();
 
 	}
 
-	public void Sendmessage(String send) {// 메시지 송신
-	}
-
-	public void Receivemessage() {// 콜백 함수로 제작하기. 추후 추가 예정
-	}
-
-	public void run() {
-		if(t[0].getId()==(Thread.currentThread()).getId()){//만약 첫번째 스레드라면 연결을 유지한다. 연결되면 두번째 스레드를 start한다.
+	public String Receivemessage() {// 콜백 함수로 제작하기. 추후 추가 예정
+		if (t[0].isAlive()) {
+			bs = null;
 			try {
-				// 소켓을 생성하여 연결을 요청한다.
-				socket = new Socket(serverip, 80);// 서버 ip, 포트
-				out = socket.getOutputStream();//송신
-				in = socket.getInputStream();//수신
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		else if(t[1].getId()==(Thread.currentThread()).getId()){//만약 두번째 쓰레드라면 수신 메시지를 확인하고, 연결 상태가 false라면 쓰레드를 종료한다.
-			
-		}
-		else{//만약 세 번째 스레드라면 메인에서 넘어온 명령으로, 송신 명령이므로 메시지를 송신하는 역할을 한다.
-			
-		}
-		switch (selection) {
-		case 1: {// Connect
-			
-		}
-		case 2: {// Disconnect
-			// Socket close
-			try {
-				socket.close();
-				System.out.println("Socket closed");
-			} catch (IOException e) {
-				System.out.println("No Socket");
-				e.printStackTrace();
-			}
-
-		}
-		case 3: {// Message Send
-			try {
-				out.write(message.length());
+				in.read(bs);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
-		case 4: {
 
+			return bs.toString();
 		}
+		return "Error";
+	}
+
+	public void run() {
+		if (t[0].getId() == (Thread.currentThread()).getId()) {// 만약 첫번째 스레드라면
+																// 연결을 유지한다.
+																// 연결되면 두번째 스레드를
+																// start한다.
+			try {
+				// 소켓을 생성하여 연결을 요청한다.
+				socket = new Socket(serverip, serverport);// 서버 ip, 포트
+				out = socket.getOutputStream();// 송신
+				in = socket.getInputStream();// 수신
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (t[1].getId() == (Thread.currentThread()).getId()) {// 만약 두번째
+																		// 쓰레드라면
+																		// 송신
+																		// 명령이다.
+			try {
+				out.write(message);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} else {// 만약 세 번째 스레드라면 Disconnect 명령이다.
+			try {
+				socket.close();
+				System.out.println("Socket closed");
+				in.close();
+				out.close();
+				System.out.println("Stream closed");
+
+			} catch (IOException e) {
+				System.out.println("No Socket or Stream");
+				e.printStackTrace();
+			}
+
 		}
 
 	}
